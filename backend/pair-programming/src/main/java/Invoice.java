c3be8e3340752d0b8def9a4c767c10e25f1df88a
Erika Silva <erikasons@gmail.com>
2020-09-29 18:57:23 -0300

    this pair-programming code challenge now is deprecated

--- backend/pair-programming/src/main/java/Invoice.java
+++
@@ -1,9 +0,0 @@
-public class Invoice {
-    private final Address billingAddress;
-    private final Address shippingAddress;
-
-    public Invoice(Order order) {
-        this.billingAddress = order.address();
-        this.shippingAddress = order.address();
-    }
-}
