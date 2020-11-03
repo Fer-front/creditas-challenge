92fa190cd2da7efa6ac2b8b6a23f2f88cfa7eea3
Erika Silva <erikasons@gmail.com>
2020-11-03 17:12:37 -0300

    create a new test for code challenge

---
+++ backend/code-challenge/src/test/java/LoanMatcherTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LoanMatcherTest {
+
+    @Test
+    void returnsPersonalLoanWhenCustomerIncomeIsUnder3000() {
+        Customer customer = new Customer(3000);
+        LoanMatcher loanMatcher = new LoanMatcher(customer);
+        List<Loan> availableLoans = loanMatcher.loans();
+
+        assertThat(availableLoans.size()).isEqualTo(1);
+
+        assertThat(availableLoans.stream().findFirst().isPresent()).isTrue();
+        assertThat(availableLoans.get(0).type()).isEqualTo("EMPRESTIMO_PESSOAL");
+    }
+}
